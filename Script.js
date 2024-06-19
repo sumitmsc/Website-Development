3b8c18cd39b7eaebcab6a245f213ea6da09a886f
sumitmsc <49392062+sumitmsc@users.noreply.github.com>
2024-06-19 23:42:27 +0530

    Add files via upload
    Basic Web page example

---
+++ Script.js
@@ -0,0 +1,7 @@
+function changeMessage() {
+  // Get a reference to the paragraph element with ID "message"
+  const messageElement = document.getElementById("message");
+
+  // Change the text content of the paragraph
+  messageElement.textContent = "This message has been updated!";
+}
